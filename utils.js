ce65f73b73b1c47f6509ff5301d448bec59936a0
abattais <alexandre-bat@hotmail.fr>
2025-05-23 14:22:38 +0000

    Update alerts

--- utils.js
+++ utils.js
@@ -81,26 +81,26 @@ export function getContentTagsDependsOnNFT(data, price, type) {
 
   if (isListing && isStandardSeason) {
     // FRANCK ONLY
-    // Listing | All seasons                          | Price <= 900
-    // Listing | All seasons                          | Price <= 9000 | Epic
+    // Listing | All seasons                          | Price <= 800
+    // Listing | All seasons                          | Price <= 8000 | Epic
     // Listing | S1                                   | Price <= 6000
     // Listing | S2                                   | Price <= 5000
     // Listing | S2 Alissa Foxy & Kelly Collins g0056 | Price <= 9000
-    // Listing | S5 Alissa Foxy g0026                 | Price <= 4500 | Rare
-    // Listing | S5 Mathilda Scorpy g0044             | Price <= 4500 | Rare
-    // Listing | S5 Shelena g0048                     | Price <= 4500 | Rare
-    // Listing | S5 Sirena Milano g0076               | Price <= 4500 | Rare
+    // Listing | S5 Alissa Foxy g0026                 | Price <= 4000 | Rare
+    // Listing | S5 Mathilda Scorpy g0044             | Price <= 4000 | Rare
+    // Listing | S5 Shelena g0048                     | Price <= 4000 | Rare
+    // Listing | S5 Sirena Milano g0076               | Price <= 4000 | Rare
     // Listing | S6                                   | Price <= 1000
-    // Listing | S6 Octokuro g0065                    | Price <= 8000
+    // Listing | S6 Octokuro g0065                    | Price <= 7000
     // Listing | S7                                   | Price <= 1000
-    if (price <= 900 ||
-      (isEpic && price <= 9000) ||
+    if (price <= 800 ||
+      (isEpic && price <= 8000) ||
       (data.season === "1" && price <= 6000) ||
       (data.season === "2" && price <= 5000) ||
       (["g0056"].includes(data.card_number) && price <= 9000) ||
-      (["g0026", "g0044", "g0048", "g0076"].includes(data.card_number) && price <= 4500 && isRare) ||
+      (["g0026", "g0044", "g0048", "g0076"].includes(data.card_number) && price <= 4000 && isRare) ||
       (data.season === "6" && price <= 1000) ||
-      (["g0065"].includes(data.card_number) && price <= 8000) ||
+      (["g0065"].includes(data.card_number) && price <= 7000) ||
       (data.season === "7" && price <= 1000)
     ) {
       return FRANCK;
