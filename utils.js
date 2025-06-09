6447065c254edac25756ba6a62bc4ee0487df2ea
abattais <alexandre-bat@hotmail.fr>
2025-06-09 19:24:26 +0000

    Update tags rules

--- utils.js
+++ utils.js
@@ -56,43 +56,37 @@ export function getContentTagsDependsOnNFT(data, price, type) {
   if (isListing && isStandardSeason) {
     // FRANCK ONLY
     // Listing | All seasons                          | Price <= 800
-    // Listing | All seasons                          | Price <= 8000 | Epic
-    // Listing | S1                                   | Price <= 6000
-    // Listing | S2                                   | Price <= 4000
-    // Listing | S2 Alissa Foxy & Kelly Collins g0056 | Price <= 9000
+    // Listing | All seasons                          | Price <= 6000 | Epic
+    // Listing | S1                                   | Price <= 5000
+    // Listing | S2                                   | Price <= 3500
     // Listing | S6                                   | Price <= 1000
-    // Listing | S6 Octokuro g0065                    | Price <= 7000
+    // Listing | S6 Octokuro g0065                    | Price <= 5000
     // Listing | S7                                   | Price <= 1000
-    // Listing | S7                                   | Price <= 4000 | Rare
+    // Listing | S7                                   | Price <= 3000 | Rare
     if (price <= 800 ||
-      (isEpic && price <= 8000) ||
-      (data.season === "1" && price <= 6000) ||
-      (data.season === "2" && price <= 4000) ||
-      (["g0056"].includes(data.card_number) && price <= 9000) ||
+      (isEpic && price <= 6000) ||
+      (data.season === "1" && price <= 5000) ||
+      (data.season === "2" && price <= 3500) ||
       (data.season === "6" && price <= 1000) ||
-      (["g0065"].includes(data.card_number) && price <= 7000) ||
+      (["g0065"].includes(data.card_number) && price <= 5000) ||
       (data.season === "7" && price <= 1000) ||
-      (data.season === "7" && price <= 4000 && isRare)
+      (data.season === "7" && price <= 3000 && isRare)
     ) {
       return FRANCK;
     }
 
     // FRANCK + NICO
-    // Listing | S7 Emiri Momota g0125 | Price <= 3000
+    // Listing | S7 Emiri Momota g0125 | Price <= 3500
     // Listing | S7 Emiri Momota g0125 | Price <= 6000 | Rare
     if (["g0125"].includes(data.card_number) &&
-      (price <= 4000 || (isRare && price <= 6000))
+      (price <= 3500 || (isRare && price <= 6000))
     ) {
       return `${FRANCK} ${NICO}`;
     }
   }
   // NICO ONLY
-  // Sale | S6 Georgia g0116      | Limited
-  // Sale | S6 Ashby Winter g0053 | Limited
-  // Sale | S6 Sakura g0119       | Limited,Rare
-  if (isSale &&
-    (isLimited && ["g0116", "g0053"].includes(data.card_number)) ||
-    (isRareOrLimited && ["g0119"].includes(data.card_number))) {
+  // S3 Kelly Zelda g0092      | Limited
+  if (["g0092"].includes(data.card_number)) {
     return NICO;
   }
   return "";
