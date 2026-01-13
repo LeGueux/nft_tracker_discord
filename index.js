bb2f9ccb9ae51272d4677e1e4553e746ddd86621
abattais <alexandre-bat@hotmail.fr>
2026-01-13 23:14:19 +0000

    ADD logs in index.js

--- index.js
+++ index.js
@@ -2,26 +2,34 @@ import 'dotenv/config';
 import express from 'express';
 import { discordClient, eventBotReady } from './discord.js';
 import { setupErrorHandlers } from './shared/error-handler.js';
+import { IS_TEST_MODE } from './dolz/config.js';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+if (!IS_TEST_MODE) {
+    const app = express();
+    const PORT = process.env.PORT || 3000;
 
-// Route HTTP simple
-app.get('/', (req, res) => {
-    console.log(
-        `APP GET '/' - ${new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' })}`,
-    );
-    res.send('Hello from Koyeb!');
-});
+    // Route HTTP simple
+    app.get('/', (req, res) => {
+        console.log(
+            `APP GET '/' - ${new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' })}`,
+        );
+        res.send('Hello from Koyeb!');
+    });
 
-// Démarrage du serveur Express
-app.listen(PORT, () => {
-    console.log(
-        `Server running on port ${PORT} - ${new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' })}`,
-    );
-});
+    // Démarrage du serveur Express
+    app.listen(PORT, () => {
+        console.log(
+            `Server running on port ${PORT} - ${new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' })}`,
+        );
+    });
+}
 
 console.log(`✅ index.js | Bot started at ${new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' })}`);
+console.log({
+    node: process.version,
+    discordJs: (await import('discord.js')).version,
+    platform: process.platform,
+});
 
 eventBotReady(discordClient);
 setupErrorHandlers(discordClient);
